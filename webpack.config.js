1ec9c610909003b55dc30f08de397fd8c5b32103
liuw <liuw@leleyuntech.com>
2017-11-21 15:04:56 +0800

    fix path

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ let webpackConfig = {
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: '/',
     filename: '[name].js?[hash]'
   },
   module: {
