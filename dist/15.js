e5460c8efd601963d15fca89cdb45e295e22d35a
liuw <liuw@leleyuntech.com>
2017-11-22 18:04:16 +0800

    fix

--- dist/15.js
+++ dist/15.js
@@ -1,2 +1,2 @@
-webpackJsonp([15],{121:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var l=t(284),r=t(1),u=r(null,l.a,!1,null,null,null);n.default=u.exports},284:function(e,n,t){"use strict";var l=function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("\n  hello world\n")])},r=[],u={render:l,staticRenderFns:r};n.a=u}});
-//# sourceMappingURL=15.js.map?4cf212ddc496aac4b7e7
+webpackJsonp([15],{121:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var l=t(285),r=t(1),u=r(null,l.a,!1,null,null,null);n.default=u.exports},285:function(e,n,t){"use strict";var l=function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("\n  hello world\n")])},r=[],u={render:l,staticRenderFns:r};n.a=u}});
+//# sourceMappingURL=15.js.map?5d5de353cabcd827e3f0
