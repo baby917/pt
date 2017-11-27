0b3346d0a3bcff0ddec718af08430b5aafdc560a
liuw <liuw@leleyuntech.com>
2017-11-27 11:06:38 +0800

    fix

--- src/view/error.vue
+++ src/view/error.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    hello world
+  <div id="error">
+    <p class="tips">长按/扫码关注公众号</p>
+    <img src="../assets/qrcode.png">
   </div>
 </template>
+
+<style lang="less">
+  #error{
+    font-size: 12px;text-align: center;
+    p{text-align: center;font-size: .16rem;color: #333;padding: .14rem 0;}
+    img{width: 60%}
+  }
+</style>
